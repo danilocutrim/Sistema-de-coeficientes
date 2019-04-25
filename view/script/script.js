90cea460e42295c908d5f5574ba88d7439031e76
Matheus <matheusalexandre.sena99@gmail.com>
2019-04-25 20:14:35 -0300

    Separação de áreas: criada a area para simulação e outra para cadastro definitivo da disciplina

--- view/script/script.js
+++ view/script/script.js
@@ -17,7 +17,31 @@ new Vue({
             O: 0
         },
         numerador: 0,
-        denominador: 0
+        numerador2: 0,
+
+        denominador: 0,
+        disc: [{
+            nome: null,
+            creditos: null
+        }, {
+            nome: "BM",
+            creditos: 4
+        }, {
+            nome: "FVV",
+            creditos: 4
+        }, {
+            nome: "CES",
+            creditos: 2
+        }, ]
+    },
+    watch: {
+        nomeDisc() {
+            let tmp = Object.keys(this.disc).filter(e => {
+                return this.disc[e].nome == this.nomeDisc;
+            });
+
+            this.creditos = this.disc[tmp].creditos;
+        }
     },
     methods: {
         async calcularCR() {
@@ -30,13 +54,23 @@ new Vue({
             this.denominador += parseInt(this.creditos);
 
             tmp = this.numerador / this.denominador
-            this.cr = tmp.toFixed(2);
-        },
-        async calcularCA() {
-
-
+            this.cr = tmp.toFixed(3);
         },
         async calcularCP() {
+            let tmp = 0;
+            let multiplicador = Object.keys(this.pesos).filter(e => {
+                return e == this.conceito;
+            });
+            if (this.pesos[multiplicador] > 0) {
+                this.numerador2 += parseInt(this.pesos[multiplicador]);
+                let denominador = 180;
+
+                tmp = this.numerador2 / denominador
+                this.cp = tmp.toFixed(3);
+            }
+
+        },
+        async calcularCA() {
 
 
         },
@@ -45,7 +79,7 @@ new Vue({
             let isValid = false;
             (this.conceito != null && this.creditos != "" && this.nomeDisc != "") ? isValid = true: isValid = false;
             //this.calcularCA();
-            //this.calcularCP();
+            this.calcularCP();
             if (isValid) {
                 this.calcularCR();
 
